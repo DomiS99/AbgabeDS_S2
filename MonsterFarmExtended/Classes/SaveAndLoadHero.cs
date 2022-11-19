e4e2c4cedceffd6ea071212432f6f5124bdb3361
Dominique Schwaiger <dominiqueschwaiger@gmx.at>
2022-11-19 17:45:44 +0100

    SaveAndLoad & Level System

---
+++ MonsterFarmExtended/Classes/SaveAndLoadHero.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Runtime.CompilerServices;
+
+namespace MonsterFarmExtended.Classes
+{
+    public class SaveAndLoadHero
+    {
+        public static void Save(string name, int age, int damage, int healthpoints, int xp)
+        {
+            List<Hero> list = new List<Hero>();
+            list.Add(new Hero(name)
+            {
+                Name = name,
+                Age = age,
+                Damage = damage,
+                Healthpoints = healthpoints,
+                XP = xp,
+
+
+            });
+
+            string json = JsonSerializer.Serialize(list);
+            File.WriteAllText(@"D:\TestSaveData\Data.json",json);
+        }
+
+        public static Hero Load()
+        {
+            string text = File.ReadAllText(@"D:\TestSaveData\Data.json");
+            Hero Player = JsonSerializer.Deserialize<Hero>(text);
+            return Player;
+        }
+    }
+}
--- MonsterFarmExtended/Program.cs
+++ MonsterFarmExtended/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using MonsterFarmExtended.Classes;
+using System.Numerics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -10,9 +11,25 @@ namespace MonsterFarmExtended
     {
         static void Main(string[] args)
         {
+
             Intro();
-            Hero Player = CreateHero();
-            Console.ReadLine();
+            Console.WriteLine($"Do you want to reload the last game (r) or create a new hero (n){Environment.NewLine}" +
+                $"Attention the last hero will be deleted if you create a new one!");
+            string RoN = Console.ReadLine();
+            if (RoN == "r")
+            {
+                Hero Player = SaveAndLoadHero.Load();
+                Console.ReadLine();
+                SaveAndLoadHero.Save(Player.Name, Player.Age, Player.Damage, Player.Healthpoints, Player.XP);
+            }
+            else if (RoN == "n")
+            {
+                Hero Player = CreateHero();
+                Console.ReadLine();
+                SaveAndLoadHero.Save(Player.Name,Player.Age,Player.Damage,Player.Healthpoints,Player.XP);
+            }
+            else
+            { Console.WriteLine("Wrong entry!"); }
         }
 
         public static void Intro()
@@ -22,6 +39,7 @@ namespace MonsterFarmExtended
             $"On your quest to glory you have to overcome some obsticals which now may seem overwhelming. But just stick in there great things have still to come{Environment.NewLine}" +
             $"You have to fight different Enemys like Monsters or Wild Animals. Dispiceable Human beings will try to stop you.{Environment.NewLine}" +
             $"But now you have to go... Oh i almost forgot: Whats your name ?");
+
         }
 
         public static Hero CreateHero()
